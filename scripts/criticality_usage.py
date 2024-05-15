745dd41b18708ffe8a41e90076206b481eaab58d
Marius Erath <marius.erath@tum.de>
2024-05-16 00:10:07 +0200

    Update usage script

--- scripts/criticality_usage.py
+++ scripts/criticality_usage.py
@@ -1,18 +1,10 @@
 from pathlib import Path
 
-from commonroad_labeling.criticality.cm_computer import compute_metrics
+from commonroad_labeling.criticality.cm_computer import CMComputer
 
-compute_metrics(str(Path.cwd().joinpath("..", "scenarios")) + "/ZAM_Over-1_1.xml")
 
-# Fail with exception in CriMe:
-# compute_metrics(str(Path.cwd().joinpath("..", "scenarios")) + "/BEL_Putte-8_2_T-1.xml")
-# compute_metrics(str(Path.cwd().joinpath("..", "scenarios")) + "/DEU_Moelln-4_4_T-1.xml")
-# compute_metrics(str(Path.cwd().joinpath("..", "scenarios")) + "/DEU_Moelln-10_2_T-1.xml")
-# compute_metrics(str(Path.cwd().joinpath("..", "scenarios")) + "/ZAM_Tjunction-1_97_T-1.xml")
+cm_computer = CMComputer(verbose=True)
+scenario_dir = Path.cwd().joinpath("..", "scenarios", "inD_converted")
+cm_computer.compute_parallel(str(scenario_dir.absolute()))
 
-# Fail with AssertionError in Reactive Planner
-# compute_metrics(str(Path.cwd().joinpath("..", "scenarios")) + "/DEU_Crit-1_1_T-1.xml")
-# compute_metrics(str(Path.cwd().joinpath("..", "scenarios")) + "/DEU_Flensburg-10_1_T-1.xml")
-# compute_metrics(str(Path.cwd().joinpath("..", "scenarios")) + "/DEU_Moabit-4_1_T-1.xml")
-# compute_metrics(str(Path.cwd().joinpath("..", "scenarios")) + "/USA_US101-5_1_T-1.xml")
-
+# compute_metrics(str(Path.cwd().joinpath("..", "scenarios", "MONA-2", "/C-DEU_MONAEast-2_1_T-199.xml")))
