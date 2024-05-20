9598c0614cd916b3e060bfbff8de20c7366083b8
Marius Erath <marius.erath@tum.de>
2024-05-20 19:59:19 +0200

    Update usage script

--- scripts/criticality_usage.py
+++ scripts/criticality_usage.py
@@ -1,10 +1,13 @@
 from pathlib import Path
 
 from commonroad_labeling.criticality.cm_computer import CMComputer
+from commonroad_crime.measure import *
 
 
-cm_computer = CMComputer(verbose=True)
-scenario_dir = Path.cwd().joinpath("..", "scenarios", "inD_converted")
+metrics = [ALatReq, ALongReq, AReq, DST, HW, TTCE, WTTR, MSD, PSD, BTN, CI, CPI, STN, LatJ,
+           LongJ, PF, P_MC, ET, PET, TET, THW, TIT, TTB, TTC, TTCStar, TTK, TTR, TTS, TTZ,
+           WTTC, SOI, DCE, ]
+
+cm_computer = CMComputer(metrics, verbose=True)
+scenario_dir = Path.cwd().joinpath("..", "scenarios", "MONA")
 cm_computer.compute_parallel(str(scenario_dir.absolute()))
-
-# compute_metrics(str(Path.cwd().joinpath("..", "scenarios", "MONA-2", "/C-DEU_MONAEast-2_1_T-199.xml")))
