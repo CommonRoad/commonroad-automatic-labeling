9ab4e2a31089a16e9a78ee543600c0402807e2e5
Marius Erath <marius.erath@tum.de>
2024-07-12 22:37:49 +0200

    Improve cm_computer.py

--- commonroad_labeling/criticality/cm_computer.py
+++ commonroad_labeling/criticality/cm_computer.py
@@ -1,23 +1,19 @@
 # Computes criticality metrics for a scenario
-import logging
-import multiprocessing
 import traceback
 from datetime import datetime
 from multiprocessing import Pool
+from pathlib import Path
 
 from commonroad.common.file_reader import CommonRoadFileReader
-from commonroad_crime.data_structure.configuration import CriMeConfiguration
+from commonroad.scenario.scenario import Scenario
 from commonroad_crime.data_structure.crime_interface import CriMeInterface
 from commonroad_crime.measure import *
-import commonroad_crime.utility.visualization as utils_vis
-from pathlib import Path
-from commonroad.scenario.scenario import Scenario
 
-from commonroad_labeling.criticality.trajectory_inserter import TrajectoryInserter
 from commonroad_labeling.criticality.crit_util import (
     compute_center_lanelet,
     find_egos_from_problem_sets,
 )
+from commonroad_labeling.criticality.trajectory_inserter import TrajectoryInserter
 
 
 class CMComputer:
@@ -28,16 +24,16 @@ class CMComputer:
         self.crime_verbose = crime_verbose
         self.overwrite = overwrite
 
-    # If no ego_id is given, the vehicle will be generated from the planing problem using reactive planner
+    # If no ego_id is given, the vehicle will be simulated from the planing problem using reactive planner
     def compute_metrics(
-        self,
-        scenario_path: str,
-        ego_id: int = None,
-        save_plots=False,
-        show_plots=False,
-        make_gif=False,
-        do_log=False,
-    ):
+            self,
+            scenario_path: str,
+            ego_id: int = None,
+            save_plots=False,
+            show_plots=False,
+            make_gif=False,
+            do_log=False,
+            output_dir=None):
         scenario, planning_problem_set = CommonRoadFileReader(scenario_path).open()
 
         if ego_id is None:
@@ -51,12 +47,12 @@ class CMComputer:
                 planning_problem_set, scenario
             )
 
-        self.compute_metrics_for_id(scenario, ego_id, scenario_path)
+        self.compute_metrics_for_id(scenario, ego_id, scenario_path, output_dir=output_dir)
 
     # computes criticality metrics
     def compute_metrics_for_id(
-        self, scenario_with_ego, ego_id, scenario_path, verbose=False
-    ):
+            self, scenario_with_ego, ego_id, scenario_path, output_dir=str(Path.cwd().joinpath("output")),
+            verbose=False):
         config = self.create_crime_config(scenario_with_ego, ego_id, scenario_path)
 
         ego_obstacle = scenario_with_ego.obstacle_by_id(ego_id)
@@ -81,8 +77,8 @@ class CMComputer:
             self.metrics, ts_start, ts_end, verbose=self.crime_verbose
         )
 
-        # TODO: make output dir variable
-        crime_interface.save_to_file(str(Path.cwd().joinpath("output")))
+        output_dir = output_dir if output_dir is not None else str(Path.cwd().joinpath("output"))
+        crime_interface.save_to_file(output_dir)
 
         if self.verbose:
             print(
@@ -92,17 +88,30 @@ class CMComputer:
         # utils_vis.plot_criticality_curve(crime_interface)
 
     def compute_parallel(
-        self, scenario_dir: str, process_count=multiprocessing.cpu_count() - 1
+            self, scenario_dir: str, process_count=2,
+            save_plots=False,
+            show_plots=False,
+            make_gif=False,
+            do_log=False,
+            output_dir:str=None
     ):
+
+        """
+        WARNING: be careful about increasing the number of processes. Depending on the amount of
+        Criticality Metrics computed, each process can take multiple GIGABYTES of RAM.
+
+        :param scenario_dir: The directory path where the scenarios are located.
+        :param process_count: The number of processes to use for parallel computation. Default value is 2.
+        :return: A list of results from the parallel computation.
+        """
         dir_path = Path(scenario_dir)
         all_scenarios = [str(x.absolute()) for x in list(dir_path.iterdir())]
         scenario_ego_pairs = []
         for scenario in all_scenarios:
             for ego_id in find_egos_from_problem_sets(scenario):
-                if not self.overwrite and Path.cwd().joinpath("output", f"CriMe-{Path(scenario).name[:-4]}_veh_{ego_id}.xml").exists():
+                if not self.overwrite and Path(output_dir).joinpath(f"CriMe-{Path(scenario).name[:-4]}_veh_{ego_id}.xml").exists():
                     continue
-                scenario_ego_pairs.append((scenario, ego_id))
-        # TODO: replace print with logging
+                scenario_ego_pairs.append((scenario, ego_id, save_plots, show_plots, make_gif, do_log, output_dir))
         print(f"Starting parallel computation of {len(scenario_ego_pairs)} tasks.")
         with Pool(processes=process_count) as pool:
             results = pool.starmap(self.compute_metrics_catching, scenario_ego_pairs)
@@ -110,23 +119,23 @@ class CMComputer:
             pool.join()
 
     def compute_metrics_catching(
-        self,
-        scenario_path: str,
-        ego_id: int = None,
-        save_plots=False,
-        show_plots=False,
-        make_gif=False,
-        do_log=False,
+            self,
+            scenario_path: str,
+            ego_id: int = None,
+            save_plots=False,
+            show_plots=False,
+            make_gif=False,
+            do_log=False,
+            output_dir=None
     ):
         try:
-            self.compute_metrics(scenario_path, ego_id, save_plots, show_plots, make_gif, do_log)
+            self.compute_metrics(scenario_path, ego_id, save_plots, show_plots, make_gif, do_log, output_dir)
         except Exception as e:
-            # TODO: Replace with logging
             print(f"{datetime.now().strftime('%H:%M:%S')}: Exception when computing metrics for scenario "
                   f"{scenario_path}, for ego_id {ego_id}.\n {traceback.format_exc()}")
 
     def create_crime_config(
-        self, scenario_with_ego: Scenario, ego_id: int, scenario_path: str
+            self, scenario_with_ego: Scenario, ego_id: int, scenario_path: str
     ):
         config = CriMeConfiguration()
         config.general.name_scenario = str(scenario_with_ego.scenario_id)
